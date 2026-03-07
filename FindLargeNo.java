dbb2420745bee243d4ba9047db4aa9759139afd8
Sandeep <sj537376@gmail.com>
2026-03-08 01:41:25 +0530

    added

--- FindLargeNo.java
+++ FindLargeNo.java
@@ -1,13 +1,27 @@
 public class FindLargeNo {
+    // public static void main(String[] args) {
+    //     int arr[]={9,3,5,1,14,13};
+    //     int max=arr[0];
+
+    //     for(int i=1;i<arr.length;i++){
+    //         if(max<arr[i]){
+    //             max=arr[i];
+    //         }
+    //     }
+    //     System.out.println("Large no:"+max);
+    // }
+
+    // find small no
+
     public static void main(String[] args) {
         int arr[]={9,3,5,1,14,13};
-        int max=arr[0];
+        int min=arr[0];
 
         for(int i=1;i<arr.length;i++){
-            if(max<arr[i]){
-                max=arr[i];
+            if(min>arr[i]){
+                min=arr[i];
             }
         }
-        System.out.println("Large no:"+max);
+        System.out.println("Small no:"+min);
     }
 }
