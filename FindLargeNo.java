a67345f3fe84a36aa6ea5edc58d401029aa9c007
vishal <sj537376@gmail.com>
2026-03-08 01:19:31 +0530

    added

---
+++ FindLargeNo.java
@@ -0,0 +1,13 @@
+public class FindLargeNo {
+    public static void main(String[] args) {
+        int arr[]={9,3,5,1,14,13};
+        int max=arr[0];
+
+        for(int i=1;i<arr.length;i++){
+            if(max<arr[i]){
+                max=arr[i];
+            }
+        }
+        System.out.println("Large no:"+max);
+    }
+}
