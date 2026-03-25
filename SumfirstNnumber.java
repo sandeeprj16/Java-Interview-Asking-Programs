d982430cb1032c2b48c36c450ed1c8b1803f9daf
sandeep <srjaiswal05102001@gmail.com>
2026-03-26 03:13:09 +0530

    added

--- SumfirstNnumber.java
+++ SumfirstNnumber.java
@@ -3,7 +3,7 @@
 
 public class SumfirstNnumber {
     public static void main(String[] args) {
-        int n=5; int sum=0;
+        int n=4; int sum=0;
         for(int i=1;i<=n;i++){
             sum=sum+i;
         }
