a79d92bb3fefc9a31482c4a6350d943395ea9c10
sandeep <srjaiswal05102001@gmail.com>
2026-03-30 23:55:25 +0530

    added

---
+++ PatternWithNo.java
@@ -0,0 +1,18 @@
+// Use while loop and print following pattern: 
+// n = 19 
+// 1 2 3 4 5 * * * * * 11 12 13 14 15 * * * * 
+
+public class PatternWithNo {
+    public static void main(String[] args) {
+        int num=19;
+        int count=1;
+        while(count<=num){
+        for(int i=0;i<5 && count<=num;i++){
+            System.out.print(count++ +" ");
+        }
+        for(int i=0;i<5 && count<=num;i++){
+            System.out.print("* ");
+            count++;
+        } }
+    }
+}
